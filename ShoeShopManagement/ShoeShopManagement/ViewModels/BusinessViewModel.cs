20d3f98d61725ffad1697c610524133eb5b798bd
thanhphat219 <phat01283249260@gmail.com>
2021-03-18 13:21:11 +0700

    UI Kinh doanh

---
+++ ShoeShopManagement/ShoeShopManagement/ViewModels/BusinessViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ShoeShopManagement.Resources.UserControls;
+using ShoeShopManagement.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ShoeShopManagement.ViewModels
+{
+    class BusinessViewModel : BaseViewModel
+    {
+        public ICommand TestCommand { get; set; }
+        public ICommand LoadCommand { get; set; }
+        public BusinessViewModel()
+        {
+            TestCommand = new RelayCommand<Button>((parameter) => true, (parameter) => Test(parameter));
+            LoadCommand = new RelayCommand<BusinessWindow>((parameter) => true, (parameter) => Load(parameter));
+            
+        }
+        public void Test(Button parameter)
+        {
+            MessageBox.Show("a");
+            
+        }
+        public void Load(BusinessWindow parameter)
+        {
+            bool checkbackground = true;
+            int a= 0;
+            for (int i = 0; i < 30; i++)
+            {
+                FieldInfoBill fieldInfoBill = new FieldInfoBill();
+                if(checkbackground)
+                {
+                    
+                    fieldInfoBill.grdMain.Background = (Brush)new BrushConverter().ConvertFromString("#EEEEEE");
+                    a++;
+                    checkbackground = false;
+                }
+                else
+                    checkbackground = true;
+                parameter.PanelBills.Children.Add(fieldInfoBill);
+            }
+        }
+    }
+}
