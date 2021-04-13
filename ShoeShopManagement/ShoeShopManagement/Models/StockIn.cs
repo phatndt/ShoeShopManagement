348c888d1844309cd7f81e75ac23b54c8131767a
nhatkb2001 <62232937+nhatkb2001@users.noreply.github.com>
2021-04-13 22:21:25 +0700

    add 5 models

---
+++ ShoeShopManagement/ShoeShopManagement/Models/StockIn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeShopManagement.Models
+{
+    class StockIn
+    {
+        private int idStockIn;
+        private DateTime dateStockIn;
+        private double total;
+
+        public int IdStockIn { get => idStockIn; set => idStockIn = value; }
+        public DateTime DateStockIn { get => dateStockIn; set => dateStockIn = value; }
+        public double Total { get => total; set => total = value; }
+        public StockIn()
+        {
+
+        }
+        public StockIn(int idStockIn,DateTime dateStockIn,double total)
+        {
+            this.idStockIn = idStockIn;
+            this.dateStockIn = dateStockIn;
+            this.total = total;
+        }
+    }
+}
